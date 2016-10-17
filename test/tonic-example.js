7f00f89cb8a3f6ae7c375735e0f0ff891d56e321
bcruddy <briancruddy@gmail.com>
2016-10-16 23:56:00 -0400

    fix tonic example, bump to 1.1.4

--- test/tonic-example.js
+++ test/tonic-example.js
@@ -8,14 +8,13 @@ q.add('packageId', '10001');
 q.add('couponCode', 'SWEETEXAMPLE120');
 console.log(q.toUrlToken());
 
-let q2 = new QueryHash(token);
+let q2 = new QueryHash(q.toUrlToken());
 console.log(q2.has('packageId'));
 console.log(q2.find('packageId'));
 q2.remove('packageId');
 console.log(q2.has('packageId'));
-console.log(q2.toQueryString());
 
-let q3 = new QueryHash(query);
+let q3 = new QueryHash(q2.toQueryString());
 console.log(q3.keys());
 
 let q4 = new QueryHash({
